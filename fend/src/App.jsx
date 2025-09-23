e8c83b3a72f3b805abd5fa9b2b5afbbee403ed68
siva-garapati <sivagarapati999@gmail.com>
2025-09-23 22:32:42 +0530

    new branch testing 23/9/25-22:32

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        this is new git branch testing
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
